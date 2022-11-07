8354101aa667de65291e4490eeedfcb2991810b2
Mads Tinko Madsen <50380211+T1NK0@users.noreply.github.com>
2022-11-07 22:41:42 +0100

    Delete H4_encryption directory

--- H4_encryption/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace H4_encryption.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
--- H4_encryption/Models/HashingViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace H4_encryption.Models
-{
-    public class HashingViewModel
-    {
-        public string Input { get; set; }
-        public string OutputHashed{ get; set; }
-
-    }
-}
